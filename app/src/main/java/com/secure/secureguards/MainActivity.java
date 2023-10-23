939b75b3891128ff59ace24c6c4b4b67c9939b08
ahmadismail <ahmadismail6162@gmail.com>
2023-10-23 17:48:18 +0100

    Firebase Implimented

---
+++ app/src/main/java/com/secure/secureguards/MainActivity.java
@@ -0,0 +1,18 @@
+package com.secure.secureguards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
