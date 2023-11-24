25809899f9600990ce81695073833f5d58e342f6
maorbeber <maorbber@gmail.com>
2023-11-24 19:27:03 +0500

    implement the navigation drawer

--- app/src/main/java/com/secure/secureguards/Dash_Board_Activity.java
+++ app/src/main/java/com/secure/secureguards/Dash_Board_Activity.java
@@ -1,10 +1,17 @@
 package com.secure.secureguards;
 
+import static com.secure.secureguards.Utils.Constant.setLoginStatus;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +20,8 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.navigation.NavigationBarItemView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.secure.secureguards.Screens.AccountActivity;
 
 public class Dash_Board_Activity extends AppCompatActivity {
 
@@ -21,6 +30,10 @@ public class Dash_Board_Activity extends AppCompatActivity {
     Home_Fragment home;
     Schedule_Fragment shift;
     Profile_Fragment profile;
+
+    private DrawerLayout drawer;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,25 @@ public class Dash_Board_Activity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.flFragment, home)
                 .commit();
+        navigationView =findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toogle=new ActionBarDrawerToggle(this,drawer, (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar),R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.logout) {
+                    setLoginStatus(Dash_Board_Activity.this,false);
+                    startActivity(new Intent(Dash_Board_Activity.this, AccountActivity.class));
+                    finish();
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
          bottomNavigationItemView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
              @Override
@@ -68,12 +100,7 @@ public class Dash_Board_Activity extends AppCompatActivity {
              }
                  });
 
-                 // setContentView(R.layout.activity_main);
-                 this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-                 getSupportActionBar().setDisplayShowCustomEnabled(true);
-                 getSupportActionBar().setCustomView(R.layout.action_bar);
-                 //getSupportActionBar().setElevation(0);
-                 View view = getSupportActionBar().getCustomView();
+
 
 
 
